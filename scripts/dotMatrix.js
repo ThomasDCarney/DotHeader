2b8669712101288fbd65efa3114a189755e8e61e
Thomas Carney <thomas@thomasdcarney.com>
2017-11-08 13:21:57 -0600

    This is the initial "simple" design with uniform background pattern. The wrapper can be of any size but the number of rows/columns specified in dotMatrix.js will determine the size of each "dot".
    I also removed the .svg files, instead using perfectly square "dots" and css background-color to do that job.

---
+++ scripts/dotMatrix.js
@@ -0,0 +1,64 @@
+var wrapper = document.getElementById("dotHeaderWrapper");
+
+// NOTE: To avoid gaps, wrapper dimensions should divide evenely with values
+// passed in as aruguments.
+var sideLength = getSideLength(60, 20);
+
+createDots();
+
+/**
+ * Since the boxes should be squares for this (change if you like) we need to
+ * use the smaller dimension so nothing gets cut off. This will also result in
+ * extra rows or columns in the other direction and possible gap at an end.
+ */
+function getSideLength(numColumns, numRows) {
+
+    var tempWidth = Math.floor(wrapper.offsetWidth / numColumns);
+    var tempHeight = Math.floor(wrapper.offsetHeight / numRows);
+
+    if(tempWidth < tempHeight) {
+
+        return tempWidth;
+
+    } else {
+
+        return tempHeight;
+
+    }
+
+} // end getSideLength
+
+
+/**
+ * This will create each "dot" or element of the matrix. It's faster and more
+ * bandwidth efficient than saving hundreds+ in the HTML.
+ */
+function createDots() {
+
+    var i, j;
+    var tempDot;
+
+    // Figure out how many rows and columns are needed.
+    var numRows = Math.floor(wrapper.offsetHeight / sideLength);
+    var numColumns = Math.floor(wrapper.offsetWidth / sideLength);
+
+    // Create the individual elements.
+    for(i = 0; i < numRows; i++) {
+
+        for(j = 0; j < numColumns; j++) {
+
+            // Create the element.
+            tempDot = document.createElement("DIV");
+            tempDot.style.height = sideLength + "px";
+            tempDot.style.width = sideLength + "px";
+            tempDot.classList.add("dot");
+            tempDot.classList.add("pinkDot");
+
+            // Add it to the line.
+            wrapper.appendChild(tempDot);
+
+        }
+
+    }
+
+} // end createDots
